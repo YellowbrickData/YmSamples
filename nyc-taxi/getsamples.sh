c7920b34c27ddcca183ef9d90e2607400ffcb69f
Kurt Westerfeld <kurt@yellowbrick.io>
2021-03-19 09:53:53 -0400

    Initial

---
+++ nyc-taxi/getsamples.sh
@@ -0,0 +1,5 @@
+curl https://s3.us-east-1.amazonaws.com/nyc-tlc/trip+data/fhv_tripdata_2019-02.csv >fhv_tripdata_2019-02.csv
+curl https://s3.us-east-1.amazonaws.com/nyc-tlc/trip+data/fhvhv_tripdata_2019-02.csv >fhvhv_tripdata_2019-02.csv
+curl https://s3.us-east-1.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2013-08.csv >green_tripdata_2013-08.csv
+curl https://s3.us-east-1.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-01.csv >yellow_tripdata_2009-01.csv
+
