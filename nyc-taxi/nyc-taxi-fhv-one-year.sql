36b98db4288a75cafc8388b40fcabdb949f64577
Kurt Westerfeld <kurt@yellowbrick.io>
2021-03-21 09:15:45 -0400

    Refactor examples into setup + load commands

--- nyc-taxi/nyc-taxi-fhv-one-year.sql
+++ nyc-taxi/nyc-taxi-fhv-one-year.sql
@@ -1,34 +1,3 @@
-DROP SCHEMA IF EXISTS nyc_taxi CASCADE;
-CREATE SCHEMA nyc_taxi;
-GRANT ALL ON SCHEMA nyc_taxi TO PUBLIC;
-
-CREATE TABLE nyc_taxi.nyc_taxi_trips_fhv (
-  "dispatching_base_num" VARCHAR(max) NULL,
-  "pickup_datetime" VARCHAR(max) NULL,
-  "dropoff_datetime" VARCHAR(max) NULL,
-  "PULocationID" VARCHAR(max) NULL,
-  "DOLocationID" VARCHAR(max) NULL,
-  "SR_Flag" VARCHAR(max) NULL
-);
-GRANT ALL ON TABLE nyc_taxi.nyc_taxi_trips_fhv TO public;
-
-CREATE EXTERNAL STORAGE nyc_taxi.nyc_taxi_storage
-  TYPE s3
-  ENDPOINT 'https://s3.us-east-1.amazonaws.com/'
-  REGION 'us-east-1';
-GRANT ALL ON EXTERNAL STORAGE nyc_taxi.nyc_taxi_storage TO public;
-
-CREATE EXTERNAL FORMAT nyc_taxi.nyc_taxi_format
-  TYPE csv
-  WITH (delimiter ',', num_header_lines 1);
-GRANT ALL ON EXTERNAL FORMAT nyc_taxi.nyc_taxi_format TO public;
-
-CREATE EXTERNAL LOCATION nyc_taxi.nyc_taxi_location
-  PATH 'nyc-tlc'
-  EXTERNAL STORAGE nyc_taxi.nyc_taxi_storage
-  EXTERNAL FORMAT nyc_taxi.nyc_taxi_format;
-GRANT ALL ON EXTERNAL LOCATION nyc_taxi.nyc_taxi_location TO public;
-
 LOAD TABLE nyc_taxi.nyc_taxi_trips_fhv
   FROM ('/trip%20data/fhv_tripdata_2019')
   EXTERNAL LOCATION nyc_taxi.nyc_taxi_location;
