cad85923572b29e7f9bda134bc1aecba4fcbb2d1
kaytch <kaytch279@gmail.com>
2021-03-27 20:01:13 -0700

    Change queries *. Add one_button_load_tpcds_sf1000.sql.
    * Associate external format with external location and remove external format from load table queries.

--- tpcds_sf1000/create_external_resource.sql
+++
@@ -1,4 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS "tpcds_sf1000";
-CREATE EXTERNAL STORAGE "tpcds_sf1000"."sample_tpcds_qa_es" TYPE S3 ENDPOINT 'https://s3.us-west-2.amazonaws.com/' REGION 'us-west-2' IDENTITY '' CREDENTIAL '';
-CREATE EXTERNAL LOCATION "tpcds_sf1000"."sample_tpcds_qa_el" PATH 'yb-sampledata-d4f1c23ea7' EXTERNAL STORAGE "tpcds_sf1000"."sample_tpcds_qa_es";
-CREATE EXTERNAL FORMAT IF NOT EXISTS "tpcds_sf1000"."sample_tpcds_qa_ef" TYPE CSV WITH (DELIMITER '|', ON_MISSING_FIELD 'SUPPLYNULL', ON_EXTRA_FIELD 'REMOVE');
--- tpcds_sf1000/load_date_dim.sql
+++ tpcds_sf1000/load_date_dim.sql
@@ -1,1 +1,1 @@
-LOAD TABLE "tpcds_sf1000"."date_dim" FROM ('tpcds-sf1000/date_dim/') EXTERNAL LOCATION "tpcds_sf1000"."sample_tpcds_qa_el" EXTERNAL FORMAT "tpcds_sf1000"."sample_tpcds_qa_ef";
+LOAD TABLE "tpcds_sf1000"."date_dim" FROM ('tpcds-sf1000/date_dim/') EXTERNAL LOCATION "tpcds_sf1000"."sample_tpcds_qa_el";
